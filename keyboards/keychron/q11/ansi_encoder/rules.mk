c0a7cdb421c66632be8fa22a494f45037f1e8acd
Nick Brassel <nick@tzarc.org>
2023-06-24 05:31:25 +1000

    Remove CORTEX_ENABLE_WFI_IDLE from keyboards. (#21353)
    * Remove CORTEX_ENABLE_WFI_IDLE from the codebase.
    
    * Turn on CORTEX_ENABLE_WFI_IDLE by default.

--- keyboards/keychron/q11/ansi_encoder/rules.mk
+++ keyboards/keychron/q11/ansi_encoder/rules.mk
@@ -1,4 +1,24 @@
+<<<<<<< HEAD
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
 SERIAL_DRIVER = usart
+||||||| parent of 416af0171c (Remove CORTEX_ENABLE_WFI_IDLE from keyboards. (#21353))
+# Build Options
+#   change yes to no to disable
+#
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
+SERIAL_DRIVER = usart
+
+# Enter lower-power sleep mode when on the ChibiOS idle thread
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+=======
+# Build Options
+#   change yes to no to disable
+#
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
+SERIAL_DRIVER = usart
+
+>>>>>>> 416af0171c (Remove CORTEX_ENABLE_WFI_IDLE from keyboards. (#21353))
