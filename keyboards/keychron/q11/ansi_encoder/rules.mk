524652a59c1c076b6d3fbec61bc9ed22bdf83598
Joel Challis <git@zvecr.com>
2023-09-12 10:09:31 +0100

    Remove duplication of STM32L432 EEPROM defaults (#21981)

--- keyboards/keychron/q11/ansi_encoder/rules.mk
+++ keyboards/keychron/q11/ansi_encoder/rules.mk
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
@@ -9,6 +10,14 @@ SERIAL_DRIVER = usart
 #
 EEPROM_DRIVER = wear_leveling
 WEAR_LEVELING_DRIVER = embedded_flash
+||||||| parent of 8975dc6c3b (Remove duplication of STM32L432 EEPROM defaults (#21981))
+# Build Options
+#   change yes to no to disable
+#
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = embedded_flash
+=======
+>>>>>>> 8975dc6c3b (Remove duplication of STM32L432 EEPROM defaults (#21981))
 SERIAL_DRIVER = usart
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
